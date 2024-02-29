cd9100bff1cc351b693ef9ca58e5be4779e69d10
Tomas Santos Yciano <102435876+tomassantos484@users.noreply.github.com>
2024-02-29 15:42:16 -0500

    Adding yogism later

--- main.py
+++ main.py
@@ -256,43 +256,4 @@ async def troutify(interaction: discord.Interaction):
         error_message = "Sorry, I couldn't fetch a statement for Mike Trout at the moment."
         await interaction.response.send_message(error_message, ephemeral=False)
 
-
-@client.tree.command(name="yogism", description="Mike Trout produces a yogism based on uploaded image!")
-async def yogism(interaction: discord.Interaction, image: discord.File):
-    
-    # Immediately defer the interaction to indicate processing is happening
-    # and to get more time for sending the response.
-    await interaction.response.defer(ephemeral=False)
-
-
-    def getResponse(model, query):
-        response = requests.post(
-            url="https://openrouter.ai/api/v1/chat/completions",
-            headers={
-                "Authorization": f"Bearer {OPENROUTER_KEY}",
-            },
-            data=json.dumps({
-                "model": model,
-                "messages": [{"role": "user", "content": query}],
-            })
-        )
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"API call failed with status code {response.status_code}")
-            return None
-        
-    # Call the function and store the response 
-    response = getResponse("gryphe/mythomist-7b:free", "Generate a random funny/semi-satiricall yogism from baseball player Yogi Berra based on the uploaded image.")
-
-    if response and 'choices' in response and len(response['choices']) > 0:
-        # Access and send the statement
-        statement = response['choices'][0]['message']['content']
-        await interaction.response.send_message(statement, ephemeral=False)
-
-    else:
-        # Handle error or empty response
-        error_message = "Sorry, I couldn't fetch a yogism at the moment."
-        await interaction.response.send_message(error_message, ephemeral=False)
-
 client.run(DISCORD_TOKEN)
